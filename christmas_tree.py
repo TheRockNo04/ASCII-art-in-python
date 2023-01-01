9b571814052839238f2d1c9acb3a708ee8361c8b
TheRockNo04 <therockno04@gmail.com>
2023-01-01 14:47:39 +0530

    Update christmas_tree.py

--- christmas_tree.py
+++ christmas_tree.py
@@ -1,22 +1,29 @@
 import random as rn
+
 height = int(input('Enter height : '))
+characters = ["✪","※","❆","❄︎","⚙︎","✵"]
 
-print(" "*height +"★")
+#Star
+print(" " * height + "★")
 
+#Tree
 for row in range(height):
     print(' ' * (height-row-1),end='')
     
-    for _ in range(row+1):
-        if row%2 == 1: print(rn.choice(['*', chr(rn.randint(33,47))]), end='')
-        else: print('-',end='')
+    for _ in range(row + 1):
+        if row%2 == 1: 
+            print(rn.choice(['*', characters[rn.randint(0, len(characters)-1)]]), end='')
+        else:
+            print('-',end='')
     
     if row%2 == 1: print(' ', end='')
     else: print('-', end='')
     
     for _ in range(row+1):
-        if row%2 == 1: print(rn.choice(['*', chr(rn.randint(33,47))]), end='')
+        if row%2 == 1: print(rn.choice(['*', characters[rn.randint(0, len(characters)-1)]]), end='')
         else: print('-',end='')
     print()
 
+#Trunk
 for _ in range(2):
-    print(" "*(height - 2) + "█"*5)
+    print(" " * (height - 2) + "█" * 5)
